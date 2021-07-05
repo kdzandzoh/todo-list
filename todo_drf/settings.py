78085c5b8bdbc871c9a110ab63d0f1e50c519ceb
Yashar Dzandzoh <kdzandzoh@gmail.com>
2021-07-05 15:15:35 -0400

    Hid secret key

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from pathlib import Path
 import os
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-74^15#0^6yzs9j+8i&480!q7p^4gtz$kd*5c)+$*na_d8j=-e8'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
